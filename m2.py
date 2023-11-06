9168bc2b39a391ae42a997c17a44d65d5bd29ffa
AliBadran716 <102072821+AliBadran716@users.noreply.github.com>
2023-11-06 22:51:03 +0200

    smoothing window

--- m2.py
+++ m2.py
@@ -1,42 +1,192 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QComboBox, QSlider, QLabel, QPushButton, QWidget
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.uic import loadUiType
+from PyQt5.QtWidgets import QApplication, QFileDialog
+import pandas as pd
+# import pyqtgraph as pg
+import sys
+import numpy as np
+from os import path
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+# Load the UI file and connect it with the Python file
+FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "smoothingWindow.ui"))
 
 
-class EqualizerUI(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Equalizer with Smoothing Windows")
+class MainApp(QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """
+        Constructor to initiate the main window in the design.
 
-        # Create a central widget and layout
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        layout = QVBoxLayout()
-        central_widget.setLayout(layout)
+        Parameters:
+        - parent: The parent widget, which is typically None for the main window.
+        """
+        super(MainApp, self).__init__(parent)
+        self.setupUi(self)
+        self.setFixedSize(750, 600)
+        self.rectangle = True
+        self.hamming = False
+        self.hanning = False
+        self.gaussian = False
+        self.handle_button()
+        # Generating a uniformly distributed signal with 100 data points between 0 and 10
+        self.signal = self.generate_signal()
+        self.visualize_window()
 
-        # Dropdown for window selection
-        self.window_selection = QComboBox()
-        self.window_selection.addItems(["Rectangle", "Hamming", "Hanning", "Gaussian"])
-        layout.addWidget(self.window_selection)
+    def handle_button(self):
+        self.window_comboBox.currentIndexChanged.connect(self.select_window)
+        self.parameter_slider.valueChanged.connect(self.visualize_window)
 
-        # Parameter controls (add more as needed)
-        self.slider = QSlider()
-        layout.addWidget(self.slider)
+    def visualize_window(self):
+        plt.clf()
+        freqs, amps = self.get_frequency_amplitude()
+        freq_range, amp_range, sti, eni = self.get_amplitudes_in_range( freqs, amps, 20, 40)
 
-        # Visualization area
-        self.visualization_label = QLabel("Visualization Area")
-        layout.addWidget(self.visualization_label)
+        selected_window = self.window_comboBox.currentText()
+        window_size = self.parameter_slider.value()
 
-        # Apply button
-        apply_button = QPushButton("Apply Window")
-        apply_button.clicked.connect(self.apply_window)
-        layout.addWidget(apply_button)
+        if selected_window == "Hamming":
+            window = self.hamming_window(window_size)
+            window_title = "Hamming Window Function"
+        elif selected_window == "Hanning":
+            window = self.hanning_window(window_size)
+            window_title = "Hanning Window Function"
+        elif selected_window == "Gaussian":
+            window = self.gaussian_window(window_size)
+            window_title = "Gaussian Window Function"
+        elif selected_window == "Rectangle":
+            window = self.rectangular_window(window_size)
+            window_title = "Rectangular Window Function"
 
-    def apply_window(self):
-        selected_window = self.window_selection.currentText()
-        # Apply the selected window function to the equalizer based on the chosen parameters
-        # Update visualization area with the modified window function
+        if window_size:
+            window = self.adjust_window_size(window, amp_range)
 
-if __name__ == "__main__":
-    app = QApplication([])
-    window = EqualizerUI()
+            amps[sti:eni + 1] = amp_range * window
+            windowed_signal = amps
+        else:
+            windowed_signal = amps
+
+        plt.plot(freqs, windowed_signal)
+        plt.title(window_title)
+        plt.xlabel("Frequency (Hz)")
+        plt.ylabel("Amplitude")
+        plt.grid()
+
+        canvas = FigureCanvas(plt.gcf())
+        canvas.draw()
+
+        scene = QGraphicsScene()
+        scene.addPixmap(QPixmap.fromImage(canvas.grab().toImage()))
+        self.graphicsView.setScene(scene)
+
+    # def parameter_slider_value_changed(self, value):
+    #     self.slider_value = value
+    #     self.visualize_window()
+
+
+    def select_window(self):
+        selected_mode = self.window_comboBox.currentText()
+        if selected_mode == 'Rectangle':
+            self.rectangle = True
+            self.hamming = False
+            self.hanning = False
+            self.gaussian = False
+            self.label.setText('Window size')
+
+
+        elif selected_mode == 'Hamming':
+            self.rectangle = False
+            self.hamming = True
+            self.hanning = False
+            self.gaussian = False
+            self.label.setText('Window size')
+
+
+        elif selected_mode == 'Hanning':
+            self.rectangle = False
+            self.hamming = False
+            self.hanning = True
+            self.gaussian = False
+            self.label.setText('Window size')
+
+
+        elif selected_mode == 'Gaussian':
+            self.rectangle = False
+            self.hamming = False
+            self.hanning = False
+            self.gaussian = True
+            self.label.setText('Window size')
+
+        self.visualize_window()
+
+    # Define window functions
+    def rectangular_window(self, window_size):
+        return np.ones(window_size)
+
+    def hamming_window(self, window_size):
+        return np.hamming(window_size)
+
+    def hanning_window(self, window_size):
+        return np.hanning(window_size)
+
+    def gaussian_window(self, window_size, sigma=1):
+        return np.exp(-0.5 * ((np.arange(window_size) - (window_size - 1) / 2) / sigma) ** 2)
+
+    # def set_label_text(self):
+    #     if self.rectangle:
+    #         self.label.setText('Window size')
+    #     elif self.hamming:
+    #
+    #     elif self.hanning:
+    #
+    #     elif self.gaussian:
+
+    def adjust_window_size(self, window, signal):
+        # Adjust window size to match the signal length
+        if len(window) != len(signal):
+            window = np.resize(window, len(signal))
+        return window
+
+    def generate_signal(self):
+        # Generating a signal with three frequencies
+        t = np.linspace(0, 1, 200, endpoint=False)  # Time points
+        freq1 = 10  # Frequency 1
+        freq2 = 30  # Frequency 2
+        freq3 = 50  # Frequency 3
+        signal = np.sin(2 * np.pi * freq1 * t) + 0.5 * np.sin(2 * np.pi * freq2 * t) + 0.2 * np.sin(
+            2 * np.pi * freq3 * t)
+        return signal
+
+    def get_amplitudes_in_range(self, freqs, amplitudes, start_freq, end_freq):
+        # Find the indices corresponding to start and end frequencies
+        start_index = np.where(freqs >= start_freq)[0][0]
+        end_index = np.where(freqs <= end_freq)[0][-1]
+
+        # Extract amplitudes within the frequency range
+        amplitudes_in_range = amplitudes[start_index:end_index + 1]
+        freqs_in_range = freqs[start_index:end_index + 1]
+
+        return freqs_in_range, amplitudes_in_range,start_index ,end_index
+
+    def get_frequency_amplitude(self):
+        N = len(self.signal)
+        T = 1 / 200  # Sample spacing (assuming 1000 samples)
+        freqs = np.fft.fftfreq(N, T)[:N // 2]  # Get the frequencies
+        fft_vals = np.fft.fft(self.signal)  # Compute FFT
+        fft_vals = 2.0 / N * np.abs(fft_vals[0:N // 2])  # Get amplitude
+
+        return freqs, fft_vals
+
+
+def main():  # method to start app
+    app = QApplication(sys.argv)
+    window = MainApp()
     window.show()
-    app.exec_()
+    app.exec_()  # infinite Loop
+
+
+if __name__ == '__main__':
+    main()
